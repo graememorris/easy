851dd6f3da0ccf1d8e992466a58f95161953fd0f
graememorris <ggraemm@gmail.com>
2026-07-09 07:38:16 +0100

    parent 6f1224bde16fb4ac7455660201fc72a1dd2eecf8 author graememorris <ggraemm@gmail.com> 1783579096 +0100 committer graeme morris <ggraemm@gmail.com> 1785659272 +0100
    this is almost certainly going to fuck everything right up...
    
    i think i'm ok...
    
    the last one was a mistake
    
    changing back to make sure ffmpeg 6 works!
    
    been fiddling about with these filescanner files for days - that's a side of the work that can easily get very complex very fast and to very little purpose... thing to do on return: a simplified scanner using both libav and libwebp for images; ditto using libav and libtags for music - but modular...
    
    some last rubbings out before switching to a db branch - this leaves the code pretty pure for scanning and its gui...
    
    some last rubbings out before switching to a db branch - this leaves the code pretty pure for scanning and its gui... (WTAF!!? i thought adding a file to the list of things to commit would be enough - clearly the fuck not!!)

--- ht/01.js
+++ ht/01.js
@@ -1,11 +1,11 @@
 {let ad=document.documentURI, re=/[0-9]\//, ind=ad.search(re); U={srv: ad.substring(0, ind+1), root: ad.substring(ind+1, ad.lastIndexOf("/")-2)}, D={a: [], v: []}};
-var a, A, jA=[], L="f", curht;//curht is the i(nteger) D.v[i] which changes on clicking to another div/class
+var a=A=jA=[], L="f", curht;//curht is the i(nteger) D.v[i] which changes on clicking to another div/class
 
 async function req(str, code){
 	var rtn, qstr="\1"+str+"\2"+code, rsp=await fetch(U.srv, {method: "put", body: qstr});
 	if (code[0]=="s" && rsp.ok) return true;
 	if (code[1]=="3") return rsp.text();
-	if (code[1]=="4"){var x=0; try{x=jA.push(await rsp.json()); a=A=jA[x-1];} catch(e){x=jA.push([["", 0, [["", 1, "<div></div>"]]]]); a=A=jA[x-1];}}
+	if (code[1]=="4") return rsp.json();
 	else return false;
 }
 
@@ -14,19 +14,7 @@ function loop(n, inc, nmin, nmax){//nmax is the length of the array NOT l-1
 	else if (inc>0){if (n+inc>=nmax) return nmin; else return n+inc;}
 	else if (inc==0) return n;
 }
-/*
-async function init(l){
-	var ad=document.documentURI, re=/[0-9]\//, ind=ad.search(re);
-	//U.srv=ad.substring(0, ind+1); U.root=ad.substring(ind+1, ad.lastIndexOf("/")+1);
-	//await req(`cat ${U.root}09tmps.json`, `m4`);
-	//for (var i=0; i<A.length; i++){document.body.insertAdjacentHTML("beforeend", A[i]);}
-	setLayout(l);
-	//i have no idea why this is working - setLayout's not returning anything - there's no suspicion of a callback - yet THIS function is letting setLayout finish without moving on...(??????????)
-	//it is an ideal place to define eventListeners such that they'll work different ways for different layouts...
-		//i wonder if maybe setLayout somehow BECOMES asynchronous if used in an asynchronous function?
-	//for (var i=0; i<D.a.length; i++){D.a[i].addEventListener("mouseup", dState);}
-}
-*/
+
 function setLayout(l){
 	var x=document.querySelectorAll("body>div");
 	for (var i=0; i<x.length; i++){
@@ -41,11 +29,14 @@ function setLayout(l){
 
 /*managedir(){
 	reads the children of D.v[1];
-	if they begin "+" it makes the file, if they begin "-" it destroys it
-	it needs to apply also to the array - so + or - causes a.splice() (preferably in the right place)
+	in layout "f":
+		if they begin "+" it makes the file, if they begin "-" it destroys it
+	in layout "t":
+		if they begin "+" it splices into that array position, if "-" it splices out
+	this is getting called from nowhere... thing to do is remove all specifics and start with the mechanics of what to do
 }*/
 async function managedir(){
-	var str=`${U.root}shell/setDa1.sh`, dstr=D.v[0].children[0].textContent, cstr="", rstr="", sep="", tstr="", ch=D.v[1].children; 
+	var str=`${U.root}shell/setDa1.sh`, dstr=D.v[0].children[0].textContent, cstr="", rstr="", sep="", tstr="", ch=D.v[1].children;
 	for (var i=0; i<ch.length; i++){
 		tstr=ch[i].textContent;
 		if (tstr[0]=="-"){
@@ -62,12 +53,25 @@ async function managedir(){
 	D.v[1].insertAdjacentHTML("afterbegin", await req(`${str} -p ${dstr} ${cstr} ${rstr}`, `m3`));
 }
 
-async function docint(path, type){
-	await req(`${U.root}shell/newfile01.sh ${path} ${type}`, `m4`);
-	setLayout(type);
-	/*
-	a=A=jA[jA.push(await req(`cat ${path}`, `m4`))-1]
-	*/
+function managearr(){
+	let ch=D.v[1].children, x=-1;
+	for (var i=0; i<ch.length; i++){
+		/*if ((x=["+", "-"].indexOf(ch[i].innerHTML[0]))>-1){
+			if (x==0){
+				var xval = x.trim.length>1 ?
+				a.splice(i, 1,)
+			}
+		}*/
+		function parseSignString(str){
+			const match=/^\s*([+-])(.*)/.exec(str);
+			if (!match) return str;
+			return {sign: match[1], remainder: match[2]};
+		}
+		function parseString(str){
+			return parseSignString(str);
+		}
+		console.log(parseString(ch[i].innerText));
+	}
 }
 
 /*getdir(){
@@ -89,25 +93,29 @@ async function getdir(e){
 				else {
 					d0a=D.v[0].textContent.split("/"); 
 					if (d0a.includes("media")) console.log(`${newstr} is a file you may want to open as a db file...`);
-					else if (d0a.includes("docs")) console.log(`${newstr} is a file you may want to open as a json file...`);
+					else if (d0a.includes("docs")){ //console.log(`${newstr} is a file you may want to open as a json file...`);
+						//console.log(await req(`cat ${newstr}`, `m4`));
+						await(A2ui("t", newstr));
+					}
 				}
 			}
 		}
 		else if (idc[0][0]=="t"){
+			//console.log(`an(chorNode)=${an}\nt(ext)=${t}\no(ffset)=${o}\nt(ext)a(rray)=${ta}\nt(ext)l(ength)=${tl}\nidc(losest)=${idc}\n`);
 			if (idc[1]=="top"){
-				for (var i=0; i<tl; i++) newstr+=`${ta[i]}/`;
-				D.v[0].children[0].innerHTML=newstr;
+				a=A;
+				for (var i=0; i<tl; i++){
+					newstr+=`${ta[i]}/`;
+					for (var j=0; j<a.length; j++){
+						if (a[j][1]==0){if (a[j][0]==ta[i]){a=a[j][2]; break;}}
+					}
+				}
+				console.log(newstr, JSON.stringify(a));
 			}
 		} 
 	}
 }
-
-function dState(e){
-	//console.log(curht, e.target.closest(`body>div[id]>div[class]`), curht==D.v[0]);
-	var i=0; for (; i<D.v.length; i++){if (curht==D.v[i]) break;} console.log(i, D.v[i]);
-	curht=e.target.closest(`body>div[id]>div[class]`);
-}
-
+/*
 function ui2a(){
 	a=A=[], d=D.v[0].innerText.split("/");
 	a.push(JSON.parse(`["${d[0]}",0]`));
@@ -143,6 +151,33 @@ function a2ui(ar, d, ch){//ar=the array, d=depth of folder, ch=child index of fi
 	D.v[1].innerHTML=hstr;
 	D.v[2].innerHTML = ar[ch][1]==1 ? ar[ch][2] : "<div></div>";
 }
+*/
+
+async function A2ui(l, file){//having imported jA[0], and a=A=jA[0], read the root directory into gui - as if you pressed the empty D.v[0] in f...
+	//and it looks like this needs to be done for each different type of document... for now, each layout using a different json pattern - t being at its simplest [["",0,[["",1,"<div></div>"]]]] - i need to specify here that if the document is made and didn't previously exist, the root array begins as that
+	//as soon as i've done this one - before going on to how text/virtual fs files get saved, i'll work out how the server is going to return the size of the file sound/made (so the difference between the two can be distinguished... or should the script for the js make a file with a template js array in it that just writes an empty page in any/every type? ones for a music database, a small image database and an image/text editor
+	//fact, this IS js - you know what layout you're in 
+	//, tail=file.substring(file.indexOf("/ht/")+4);
+	
+	let x=0, hstr="", top=""; try{x=jA.push(await req(`cat ${file}`, `m4`)); a=A=jA[x-1];} catch(e){x=jA.push([{name:"", type: 0, data: [{name: "", type: 1, data: "<div></div>"}]}]); a=A=jA[x-1];}
+	//console.log(JSON.stringify(a), JSON.stringify(A), JSON.stringify(jA));
+	//the assumption here is that a complex array has to exist because we have a complex DOM
+		//better to assume - as usual - as always - that a[] needs to be reset: 
+			//[["",1,"<div></div>"]] is fine as a definition of the bottom two divs, and there is no need to fill out the top if you use the filename as a constant:
+			
+			
+	top = a.length!=1 ? "" : `${a[0].name.length==0 ? "0" : a[0].name}/`;
+	a=a[0].data;
+	for (var i=0; i<a.length; i++){
+		let isdir="", name="";
+		if (a[i].type==0) isdir="/";
+		if (a[i].name.length==0) name=`${i}`; else name=a[i].name;
+		hstr+=`<div>${name}${isdir}</div>`;
+	}
+	setLayout("t");
+	D.v[0].children[0].innerHTML=top;
+	D.v[1].innerHTML=hstr;
+}
 
 function m(e){
 	getdir(e);
@@ -163,43 +198,26 @@ function kd(e){
 
 //var editor=document.getElementById('my-editor');
 
+function writeA(e){
+	//const time = new Date().toLocaleTimeString();
+  //console.log(`[${time}] ${e.target.tagName} (${e.target.textContent.trim()}) lost consciousness...`);
+  if (e.target==D.v[0]) console.log(e.target.textContent.split("/"));
+  if (e.target==D.v[1]) console.log(e.target.innerHTML);
+  if (e.target==D.v[2]) console.log(e.target.innerHTML);
+}
+
 function paste(e){
 	e.preventDefault();
 	var dp=e.clipboardData.getData("text/plain"),dh=e.clipboardData.getData("text/HTML")
 	console.log(dp); console.log(dh);
-	/*
-D.a[5].addEventListener('paste', (event) => {
-    // 1. Prevent the default browser rich-text paste action
-    event.preventDefault();
-
-    // 2. Fetch data from clipboard strictly as plain text (drops all HTML/CSS)
-    let rawText = (event.clipboardData || window.clipboardData).getData('text/plain');
-    
-		console.log(rawText)
-    
-    
-
-    // 3. Purge all tabs (\t), newlines (\n), and carriage returns (\r)
-    // This replaces them with a single standard space
-    rawText = rawText.replace(/[\t\n\r]+/g, ' ');
-
-		console.log(rawText)
-    // 4. Safely insert the fully sanitized string at the current cursor position
-    const selection = window.getSelection();
-    if (!selection.rangeCount) return;
-    
-    selection.deleteFromDocument();
-    selection.getRangeAt(0).insertNode(document.createTextNode(rawText));
-    
-    // 5. Collapse the cursor to the end of the newly pasted text
-    selection.collapseToEnd();
-    */
 }
 
 
 window.addEventListener("load", function(){setLayout("f");})
 window.addEventListener("mousedown", m);
 window.addEventListener("mouseup", m);
+window.addEventListener("focusout", writeA);
+window.addEventListener("focusin", writeA);
 window.addEventListener("keydown", kd);
 window.addEventListener("paste", paste);
 
---
+++ shell/src/anim2db02.c
@@ -0,0 +1,209 @@
+// compile with makefile @ ../
+// program requires two parameters: the database file to read/write, and the query to use to filter which records to process
+// it would be nice to make it sufficiently modular that a revision of some idtags in a music database could use the same framework as a set of dimensions for small image and animation files
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>      // For usleep/sleep throttling and getopt
+#include <sqlite3.h>     // SQLite3 C API
+
+#include "getanim.h"
+
+int setup_statements(sqlite3 *db, sqlite3_stmt **select_stmt, sqlite3_stmt **insert_stmt, sqlite3_stmt **update_stmt, const char *scan_dir) {
+    // Modify select SQL if a specific directory path filtering is provided
+    char select_sql[2048];
+    if (scan_dir != NULL) {
+        // Safe string formatting for target directory filtering
+        snprintf(select_sql, sizeof(select_sql),
+            "SELECT f.id, f.path, f.name FROM files f "
+            "LEFT JOIN dims d ON f.id = d.id "
+            "WHERE d.id IS NULL AND f.path = '%s' LIMIT 1;", scan_dir);
+    } else {
+        strcpy(select_sql, 
+            "SELECT f.id, f.path, f.name FROM files f "
+            "LEFT JOIN dims d ON f.id = d.id "
+            "WHERE d.id IS NULL LIMIT 1;");
+    }
+
+    // Insert metadata into the dims table
+    const char *insert_sql = 
+        "INSERT OR REPLACE INTO dims (id, width, height, n_frames, codec, n_video_streams, n_audio_streams) "
+        "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+    // If a file is completely unreadable or broken, insert placeholder values 
+    const char *update_sql = 
+        "INSERT OR REPLACE INTO dims (id, width, height, n_frames, codec, n_video_streams, n_audio_streams) "
+        "VALUES (?, -1, -1, -1, 'FAILED', 0, 0);";
+
+    if (sqlite3_prepare_v2(db, select_sql, -1, select_stmt, NULL) != SQLITE_OK ||
+        sqlite3_prepare_v2(db, insert_sql, -1, insert_stmt, NULL) != SQLITE_OK ||
+        sqlite3_prepare_v2(db, update_sql, -1, update_stmt, NULL) != SQLITE_OK) {
+        fprintf(stderr, "SQL Statement Preparation Failed: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+    return 1;
+}
+
+int process_single_file(sqlite3 *db, sqlite3_stmt *select_stmt, sqlite3_stmt *insert_stmt, sqlite3_stmt *update_stmt) {
+    int result = 0; // 0 = no files, 1 = file processed, -1 = database error
+
+    // Execute the query to find a pending file path
+    int rc = sqlite3_step(select_stmt);
+    
+    if (rc == SQLITE_ROW) {
+        int file_id = sqlite3_column_int(select_stmt, 0);
+        const char *file_path_dir = (const char *)sqlite3_column_text(select_stmt, 1);
+        const char *file_name = (const char *)sqlite3_column_text(select_stmt, 2);
+        
+        // Create a buffer to hold the combined full path
+        char full_path[4096]; 
+        snprintf(full_path, sizeof(full_path), "%s/%s", file_path_dir, file_name);
+        
+        printf("Safely reading metadata: %s\n", full_path);
+        
+        struct AnimationProperties props;
+        memset(&props, 0, sizeof(struct AnimationProperties));
+
+        // Call your existing function inside AnimationAnalyzer.c
+        int scan_status = extract_animation_properties(full_path, &props);
+
+        // Secure database transactions to protect integrity
+        sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, NULL);
+
+        if (scan_status == 0) {
+            sqlite3_bind_int(insert_stmt, 1, file_id);
+            sqlite3_bind_int(insert_stmt, 2, props.width);
+            sqlite3_bind_int(insert_stmt, 3, props.height);
+            sqlite3_bind_int(insert_stmt, 4, props.frame_count); 
+            sqlite3_bind_text(insert_stmt, 5, props.codec_name, -1, SQLITE_TRANSIENT); 
+            sqlite3_bind_int(insert_stmt, 6, props.video_stream_count); 
+            sqlite3_bind_int(insert_stmt, 7, props.audio_stream_count); 
+
+            sqlite3_step(insert_stmt);
+            sqlite3_reset(insert_stmt);
+            sqlite3_clear_bindings(insert_stmt);
+        } else {
+            fprintf(stderr, "WARNING: File unreadable or corrupted. Flagging ID %d.\n", file_id);
+            sqlite3_bind_int(update_stmt, 1, file_id);
+            sqlite3_step(update_stmt);
+            sqlite3_reset(update_stmt);
+            sqlite3_clear_bindings(update_stmt);
+        }
+        sqlite3_exec(db, "COMMIT;", NULL, NULL, NULL);
+        result = 1; // File processed successfully
+    } else if (rc == SQLITE_DONE) {
+        result = 0; // No pending files left in the queue
+    } else {
+        result = -1; // Database error occurred
+    }
+    sqlite3_reset(select_stmt);
+    return result;
+}
+
+void finalize_statements(sqlite3_stmt *select_stmt, sqlite3_stmt *insert_stmt, sqlite3_stmt *update_stmt) {
+    if (select_stmt) sqlite3_finalize(select_stmt);
+    if (insert_stmt) sqlite3_finalize(insert_stmt);
+    if (update_stmt) sqlite3_finalize(update_stmt);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc!=3){printf("usage: %s <database> <filter>\n", argv[0]); return 0;}
+    char *db_path = NULL;
+    char *scan_dir = NULL;
+    int wipe_records = 0;
+    int opt;
+
+    // Parse command line arguments using getopt
+    while ((opt = getopt(argc, argv, "f:d:wh")) != -1) {
+        switch (opt) {
+            case 'f':
+                db_path = optarg;
+                break;
+            case 'd':
+                scan_dir = optarg;
+                break;
+            case 'w':
+                wipe_records = 1;
+                break;
+            case 'h':
+                print_usage(argv[0]);
+                return 0;
+            default:
+                print_usage(argv[0]);
+                return 1;
+        }
+    }
+
+    // Database file configuration check
+    if (db_path == NULL) {
+        fprintf(stderr, "FATAL: Database file (-f) must be specified.\n");
+        print_usage(argv[0]);
+        return 1;
+    }
+
+    sqlite3 *db;
+    sqlite3_stmt *select_stmt = NULL;
+    sqlite3_stmt *insert_stmt = NULL;
+    sqlite3_stmt *update_stmt = NULL;
+
+    // Open your SQLite database file dynamically
+    if (sqlite3_open(db_path, &db) != SQLITE_OK) {
+        fprintf(stderr, "FATAL: Cannot open database %s: %s\n", db_path, sqlite3_errmsg(db));
+        return 1;
+    }
+
+    // Ensure the 'dims' table exists
+    const char *create_table_sql = 
+        "CREATE TABLE IF NOT EXISTS dims ("
+        "  id INTEGER PRIMARY KEY," 
+        "  width INTEGER,"
+        "  height INTEGER,"
+        "  n_frames INTEGER,"
+        "  codec TEXT,"
+        "  n_video_streams INTEGER,"
+        "  n_audio_streams INTEGER,"
+        "  FOREIGN KEY(id) REFERENCES files(id)"
+        ");";
+    
+    if (sqlite3_exec(db, create_table_sql, NULL, NULL, NULL) != SQLITE_OK) {
+        fprintf(stderr, "FATAL: Failed to verify 'dims' table: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return 1;
+    }
+
+    // Wipe existing data if specified by user (-w flag)
+    if (wipe_records) {
+        printf("Flag -w passed. Wiping existing rows inside table 'dims'...\n");
+        if (sqlite3_exec(db, "DELETE FROM dims;", NULL, NULL, NULL) != SQLITE_OK) {
+            fprintf(stderr, "WARNING: Could not wipe records: %s\n", sqlite3_errmsg(db));
+        }
+    }
+
+    // Prepare SQL queries with potential path limitation passed down
+    if (!setup_statements(db, &select_stmt, &insert_stmt, &update_stmt, scan_dir)) {
+        sqlite3_close(db);
+        return 1;
+    }
+
+    printf("Visual Inspector engine initialized. Processing queue safely...\n");
+
+    // Main background loop
+    while (1) {
+        int file_processed = process_single_file(db, select_stmt, insert_stmt, update_stmt);
+        
+        if (file_processed == 1) {
+            usleep(250000); 
+        } else if (file_processed == 0) {
+            sleep(5);
+        } else {
+            fprintf(stderr, "Encountered critical database error. Halting.\n");
+            break;
+        }
+    }
+
+    finalize_statements(select_stmt, insert_stmt, update_stmt);
+    sqlite3_close(db);
+    return 0;
+}
